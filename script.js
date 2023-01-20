542cb5f80bbf8b651eedf3dde76389a1e397364e
taras <tarik2002sta@gmail/com>
2023-01-20 14:18:00 +0200

    start gitignore

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let arr = +prompt('write a length');
-let newArray = new Array(arr);
-let count = [];
-for (let i = 0; i < newArray.length; i++) {
-    const num = +prompt('Введите число');
-    count[i] = num;
+const arr = +prompt('write a length');
+const newArray = new Array(arr);
+const count = [];
+for (let i = 0; i < newArray.length; i += 1) {
+  const num = +prompt('Введите число');
+  count[i] = num;
 }
 
 console.log(count);
-console.log(count.sort((a,b) => a-b));
-count.splice(1,3);
-console.log(count)
+console.log(count.sort((a, b) => a - b));
+count.splice(1, 3);
+console.log(count);
